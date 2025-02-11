7a2c40d69c5b6b6562bf488e64bbf8badc9491bb
Sunil Kumar K V <sunilkumarkv@Sunils-MacBook-Pro.local>
2025-02-11 22:38:19 +0530

    Added Campus, Contact section and some changes in Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -16,11 +17,11 @@ const Navbar = () => {
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
         <img src={logo} alt="" className='logo'/>
         <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
+            <li><Link activeClass="active" to="hero" spy={true} smooth={true} offset={0} duration={500}>Home</Link></li>
+            <li><Link activeClass="active" to="hero" spy={true} smooth={true} offset={0} duration={500}>Program</Link></li>
+            <li><Link activeClass="active" to="hero" spy={true} smooth={true} offset={0} duration={500}>About us</Link></li>
+            <li><Link activeClass="active" to="hero" spy={true} smooth={true} offset={0} duration={500}>Campus</Link></li>
+            <li><Link activeClass="active" to="hero" spy={true} smooth={true} offset={0} duration={500}>Testimonials</Link></li>
             <li><button className='btn'>Contact us</button></li>
         </ul>
     </nav>
